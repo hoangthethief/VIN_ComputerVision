b48bd17b67dcb521df06af08f0c7e71ff3c69ba0
quangtung26 <quangtung2681@gmail.com>
2023-09-24 02:45:31 +0700

    dataset loader

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -4,15 +4,35 @@ import pandas as pd
 from numpy.random import randint
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
-from PIL import Image
+import cv2
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
 
 
 
 class LandscapeDataset(Dataset):
-    def __init__(self, metadata_path, transform=None, test_mode=False):
+    def __init__(self, metadata_path, test_mode=False):
         self.metadata = pd.read_csv(metadata_path)
-        self.transform = transform
         self.test_mode = test_mode
+
+
+        self.transform = A.Compose([
+                                A.SmallestMaxSize(max_size=360),
+                                A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
+                                A.RandomCrop(height=256, width=256),
+                                A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+                                A.RandomBrightnessContrast(p=0.5),
+                                A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+                                ToTensorV2()
+                                ])
+
+        if test_mode:
+            self.transform = A.Compose([
+                                    A.SmallestMaxSize(max_size=360),
+                                    A.CenterCrop(height=256, width=256),
+                                    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+                                    ToTensorV2()])
 
 
     def __len__(self):
@@ -21,11 +41,13 @@ class LandscapeDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.metadata.iloc[idx, 0]
-        img = Image.open(img_path).convert('RGB')
+        
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         label = self.metadata.iloc[idx, 1]
 
-        if self.transform:
-            sample = self.transform(sample)
+
+        img = self.transform(image=img)['image']
 
         return [img, label]
     
@@ -33,10 +55,9 @@ class LandscapeDataset(Dataset):
 if __name__ == '__main__':
     metadata_path = 'data/metadata.csv'
     dataset = LandscapeDataset(metadata_path)
+    data_loader = DataLoader(dataset, batch_size=8, shuffle=True)
 
-    # data_loader = DataLoader(dataset, batch_size=8, shuffle=None)
     c = 0
-    for x, y in dataset:
-        c += 1
-        print(type(x), y)
+    for x, y in data_loader:
+        print(x.shape, y)
         break
